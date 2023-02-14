f19d269c6e1dee1688da09799203ce7e672d0421
tet-n <114179657+tet-n@users.noreply.github.com>
2023-02-14 20:01:41 +0000

    Deploying to gh-pages from @ tet-n/goit-js-hw-09@414c9b262132d2be947dc1a6d198c3dbb9058471 🚀

---
+++ 01-color-switcher.c846d487.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;function n(){e.disabled?(e.disabled=!1,t.disabled=!0):(e.disabled=!0,t.disabled=!1)}function a(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.disabled=!0,e.addEventListener("click",(function(){a(),d=setInterval((()=>{a()}),1e3),n()})),t.addEventListener("click",(function(){clearInterval(d),n()}));
+//# sourceMappingURL=01-color-switcher.c846d487.js.map
