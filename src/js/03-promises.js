384cfe4745e0b199081d3ab41177c87904c577aa
Tetiana Nivchyk <tata.02od@gmail.com>
2023-02-17 00:12:53 +0200

    finished 3rd task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  // Отримуємо значення полів форми
+  let { amount, step, delay } = e.currentTarget.elements;
+  const obj = {
+    amount: Number(amount.value),
+    step: Number(step.value),
+    delay: Number(delay.value),
+  };
+
+  generatePromises(obj);
+}
+
+// Функція генерування промісів
+function generatePromises({ amount, step, delay } = {}) {
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
   }
 }
+
+// Функція повернення проміса
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+// Слухач події
+form.addEventListener('submit', onFormSubmit);
